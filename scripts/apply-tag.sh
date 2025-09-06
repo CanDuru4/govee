09f0cfe1b78cb76ed55970f125eba204e1598ee7
CanDuru4 <canduru2004@gmail.com>
2025-09-05 21:55:02 -0400

    fix: macOS sed in apply-tag.sh

--- scripts/apply-tag.sh
+++ scripts/apply-tag.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 # This script updates the version number for
 # the addon based on the current commit timestamp
+
+set -eu
+
 TAG_NAME=${TAG_NAME:-$(git -c "core.abbrev=8" show -s "--format=%cd-%h" "--date=format:%Y.%m.%d")}
-sed -i "s/version:.*/version: \"$TAG_NAME\"/" addon/config.yaml
+
+CONFIG_FILE="addon/config.yaml"
+
+# Use a portable in-place sed invocation (BSD vs GNU)
+case "$(uname -s)" in
+  Darwin*)
+    sed -i "" -e "s/^version:.*/version: \"$TAG_NAME\"/" "$CONFIG_FILE"
+    ;;
+  *)
+    sed -i -e "s/^version:.*/version: \"$TAG_NAME\"/" "$CONFIG_FILE"
+    ;;
+esac
